5f0ca671052e43f4d6547ce64d4771c0c95ea128
olajide olamide <olajideseyi8181@gmail.com>
2022-07-17 18:39:29 +0100

    second commit, removed every unnecessary codes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./index.css";
 import Birthday from "./Birthday/Birthday";
-// import infos from "./stays";
-// import Room from "./Room";
 
 const App = () => {
   return (
     <article className="container">
-      {/* <section className="nav"> */}
       <section>
         <h1 className="text-center">BIRTHDAY REMINDER</h1>
-        {/* <img src="./imgs/logo.png" alt="" />
-        <input type="search" name="search" id="" /> */}
       </section>
       <section className="main">
         <Birthday />
